317329bee4a934b207374f607b479308fec9f570
YYHDBL <youyihui11@163.com>
2023-02-28 19:14:34 +0800

    菜品管理完善完成 可以显示分类名称  但是没搞懂怎么实现的

--- src/main/java/com/yyhdbl/controller/DishController.java
+++ src/main/java/com/yyhdbl/controller/DishController.java
@@ -8,13 +8,17 @@ import com.yyhdbl.dto.DishDto;
 import com.yyhdbl.entity.Category;
 import com.yyhdbl.entity.Dish;
 import com.yyhdbl.entity.DishFlavor;
+import com.yyhdbl.service.CategoryService;
 import com.yyhdbl.service.DishFlavorService;
 import com.yyhdbl.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品管理
@@ -27,6 +31,8 @@ public class DishController {
     private DishService dishService;
     @Autowired
     private DishFlavorService dishFlavorService;
+    @Autowired
+    private CategoryService categoryService;
 
     /**
      * 新增菜品
@@ -55,12 +61,33 @@ public class DishController {
     public R<Page> page(int page, int pageSize, String name) {
         log.info("page ={},pageSize={},name={}", page, pageSize, name);
         //构建分页构造器
-        Page pageinfo = new Page(page, pageSize);
+        Page<Dish> pageinfo = new Page(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
         //构造条件构造器
         LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper();
+        lambdaQueryWrapper.like(name != null, Dish::getName, name);
         //按照sort排序
-        lambdaQueryWrapper.orderByAsc(Dish::getSort);
+        lambdaQueryWrapper.orderByAsc(Dish::getUpdateTime);
+
         dishService.page(pageinfo, lambdaQueryWrapper);
-        return R.success(pageinfo);
+        //由于页面想要展示的categoryname在dish中没有 存在于dishDto中 想要将其拷贝过去
+        BeanUtils.copyProperties(pageinfo, dishDtoPage, "records");
+        List<Dish> records = pageinfo.getRecords();
+
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto);
+
+            Long categoryId = item.getCategoryId();
+            //根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            String categoryName = category.getName();
+dishDto.setCategoryName(categoryName);
+            return dishDto;//完全听不懂
+        }).collect(Collectors.toList());
+
+
+        dishDtoPage.setRecords(list);
+        return R.success(dishDtoPage);
     }
 }
