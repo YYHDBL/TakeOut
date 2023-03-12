7bcec28c95955be84fb37eb97c2fc16630920928
YYHDBL <youyihui11@163.com>
2023-03-12 18:52:12 +0800

    购物车添加完成

--- src/main/java/com/yyhdbl/controller/ShoppingCartController.java
+++ src/main/java/com/yyhdbl/controller/ShoppingCartController.java
@@ -1,6 +1,8 @@
 package com.yyhdbl.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.yyhdbl.common.BaseContext;
 import com.yyhdbl.common.R;
 import com.yyhdbl.entity.ShoppingCart;
 import com.yyhdbl.service.ShoppingCartService;
@@ -17,10 +19,43 @@ public class ShoppingCartController {
     private ShoppingCartService shoppingCartService;
 
     @PostMapping("/add")
-    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart)
-    {
-        log.info("{}",shoppingCart);
-        return null;
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        log.info("{}", shoppingCart);
+
+        //设置用户id 指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        //查询当前菜品或者套餐是否在购物车中
+        Long dishId = shoppingCart.getDishId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+        if (dishId != null) {
+            //添加到购物车的是菜品
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            //添加到购物车的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+        ShoppingCart shoppingCartOne = shoppingCartService.getOne(queryWrapper);
+
+        //如果已经在购物车中 则不需要重复插入 直接修改num值
+        if (shoppingCartOne != null) {
+            Integer number = shoppingCartOne.getNumber();
+            shoppingCartOne.setNumber(number+1);
+            shoppingCartService.updateById(shoppingCartOne);
+        }
+        //如果不存在 则添加到购物车 num默认就是1
+        else {
+            shoppingCart.setNumber(1);
+            shoppingCartService.save(shoppingCart);
+            shoppingCartOne = shoppingCart;
+        }
+
+        return R.success(shoppingCartOne);
     }
+
+
+
 
 }
